c523d90f63cfad2d1536741f49a4fcf44d895cb3
JosueFuen <fosuedavid@yahoo.com.ar>
2026-05-22 19:03:38 +0000

    Finalizados

---
+++ Fuentes_Josue_TP4_PO/src/Ejercicio_04/Main.java
@@ -0,0 +1,5 @@
+package Ejercicio_04;
+
+public class Main {
+
+}
---
+++ Fuentes_Josue_TP4_PO/src/Ejercicio_04/Producto.java
@@ -0,0 +1,10 @@
+package Ejercicio_04;
+
+public class Producto {
+    private String nombre;
+    private double precioBase=0.0;
+    private static double IVA=0.21;
+    
+
+
+}
